51052ec7591bba0a02f5827023c19b16bf8a7b56
Raahel Baig <17131a05h5@gvpce.ac.in>
2020-04-07 15:41:14 +0530

    split.py update

--- split.py
+++ split.py
@@ -1,12 +1,22 @@
-fileName = input("File to split: ")                        # Which file to split?
-extension = fileName.split('.')[-1]
-specificLines = int(input("Specify lines to split by: "))  # How many lines to split by?
-count = 0                                                  # Count to keep track of lines written to a file
-fileNumber = 1                                             # To name multiple new files
-with open(fileName, 'r') as rf:                            # Reading the input file
-    for line in rf:                                        # Reading each line
-        wf = open(f'file{fileNumber}.{extension}', 'a+')   # Create and write if doesn't exist/ Append if exists
-        wf.write(line)                                     # Writing the line
-        count += 1                                         # Incrementing the no. of lines written to file
-        if count % specificLines == 0:                     # If count is a multiple of specified line,
-            fileNumber += 1                                # create a fresh file for rest of the lines
+def split(file, lines):
+    extension = file.split('.')[-1]                                 # Extracting file extension
+    name = file.split('.')[0]
+    count = 0                                                       # Count to keep track of lines written to a file
+    file_number = 1                                                 # To name multiple new files
+    with open(file, 'r') as rf:                                     # Reading the input file
+        for line in rf:                                             # Reading each line
+            wf = open(f'{name}child{file_number}.{extension}', 'a+')  # Create & write if doesn't exist/append if exists
+            wf.write(line)                                          # Writing the line
+            count += 1                                              # Incrementing the no. of lines written to file
+            if count % lines == 0:                                  # If count is a multiple of specified line,
+                file_number += 1                                    # create a fresh file for rest of the lines
+
+
+def main():
+    file_name = input("File to split: ")                            # Which file to split?
+    specific_lines = int(input("Specify lines to split by: "))      # How many lines to split by?
+    split(file_name, specific_lines)                                # Function call
+
+
+if __name__ == "__main__":
+    main()
