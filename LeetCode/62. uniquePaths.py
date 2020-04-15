1619333473e0292e4d6e55113857f73027eb455b
Raahel Baig <17131a05h5@gvpce.ac.in>
2020-04-15 23:37:54 +0530

    LC 62 solution optimized

--- LeetCode/62. uniquePaths.py
+++ LeetCode/62. uniquePaths.py
@@ -1,11 +1,7 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        each = [0 for x in range(m)]
+        each = [1 for x in range(m)]
         paths = [each for x in range(n)]
-        for i in range(n):
-            paths[i][0] = 1
-        for i in range(m):
-            paths[0][i] = 1
         for i in range(1, n):
             for j in range(1, m):
                 paths[i][j] = paths[i-1][j] + paths[i][j-1]
