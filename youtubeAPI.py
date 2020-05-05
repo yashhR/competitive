07b47d4ebfb7952a1970db86968f358ce7c443cf
Raahel Baig <17131a05h5@gvpce.ac.in>
2020-05-05 18:08:48 +0530

    YT API for viewCount

---
+++ youtubeAPI.py
@@ -0,0 +1,44 @@
+'''
+https://www.googleapis.com/youtube/v3/videos?part=statistics&id=GVGmKoQ_MPI&key=AIzaSyAPqvy4xQqjkaTZZIGwMppQ9S5D3UKBK4E
+'''
+# -*- coding: utf-8 -*-
+
+# Sample Python code for youtube.videos.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/guides/code_samples#python
+
+import os
+import json
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+
+
+scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+def main():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
+
+    # Get credentials and create an API client
+    # flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+    #     client_secrets_file, scopes)
+    # credentials = flow.run_console()
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey="AIzaSyAPqvy4xQqjkaTZZIGwMppQ9S5D3UKBK4E")
+
+    request = youtube.videos().list(
+        part="snippet,contentDetails,statistics",
+        id="GVGmKoQ_MPI"
+    )
+    response = request.execute()
+    print(response)
+    views = response['items'][0]['statistics']['viewCount']
+    print(views)
+if __name__ == "__main__":
+    main()
